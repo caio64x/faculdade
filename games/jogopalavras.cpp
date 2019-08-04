484c7a10456771375e56159276c956f8b533a7b1
caio <caiolimeira10@hotmail.com>
2019-08-04 13:26:46 -0300

    implementacoes

--- games/jogopalavras.cpp
+++ games/jogopalavras.cpp
@@ -6,38 +6,61 @@
 #include <time.h>
 using namespace std;
 
-//limpa tela
+//funçao limpa tela
 void limpaTela(){ 
     system("CLS");   }
 
+//funcao palavra aleatoria
 string rotornaPalavraAleatoria(){
-string palavras[3] = {"abacaxi", "manga", "Morango"};
+string palavras[3] = {"abacaxi", "manga", "morango"};
 
 //indice gerado
-    int indiceAleatorio = rand() %3;
+          int indiceAleatorio = rand() %3;
 //retorna a palavra
-        return palavras[indiceAleatorio];}
+            return palavras[indiceAleatorio];}
 
-void jogarSozinho(){
-
-//palavra a ser advinhada
-        string palavra = rotornaPalavraAleatoria();
-            cout << " A plavra secreta eh " << palavra << "\n";
-//tamanho da palavra
-int tamanhoDaPalavra = palavra.size();
-
-int cont = 0;
+//funcao de mascara
+string retornaPalavraComMascara(string palavra, int tamanhoDaPalavra){
+    //declaracao de variaveis
+    int cont = 0;
 string palavraComMascara;
-
 while(cont < tamanhoDaPalavra){
     palavraComMascara +="_";
-    cont ++;
-        cout << "A palavra secreta eh: " << palavra <<"(tamanho:"<< tamanhoDaPalavra << ")";
-            cout << "\nMascara:" << palavraComMascara;
-}}
+    cont ++;}
+    return palavraComMascara;}
 
+//funçao opcao jogar sozinho
+void jogarSozinho(){
+        //palavra a ser advinhada
+        string palavra = rotornaPalavraAleatoria();
+//tamanho da palavra
+int tamanhoDaPalavra = palavra.size();
+// retorno da palavra e o tamanho dela para a funcao etornaPalavraComMascara
+string palavraComMascara = retornaPalavraComMascara(palavra, tamanhoDaPalavra);
+
+//declaracao de variaveis
+int tentativas = 0, maximoDeTentativas = 5, cont = 0;
+//letra identificada
+char letra;
+    //sistema de tentativas
+    while (maximoDeTentativas - tentativas > 0){
+        cout << "Palavra: " << palavraComMascara <<"(tamanho:"<< tamanhoDaPalavra << ")";
+                 cout << "\nTentativas restantes:" << maximoDeTentativas - tentativas << "\n\n";
+//leitura de letras
+         cout << "Digite uma letra:";
+           cin >> letra;
+//percorre a palavra se acertar a letra
+for (cont = 0; cont < tamanhoDaPalavra; cont++){
+    if (palavra[cont] == letra){
+        //faço a letra aparecer na palavra
+        palavraComMascara[cont] = palavra[cont]; }}
+    //incrementa tentativas
+  tentativas ++;}}
+
+//funçao menu
 void menuInicial(){
 // opcao do usuario
+//declaracao de variaveis
 int opcao = 0;
 //menu
 while (opcao < 1 || opcao > 3){
@@ -47,30 +70,25 @@ cout << "Bem vindo ao jogo";
     cout << "\n1 - Jogar";
         cout << "\n2 - Sobre";
             cout << "\n3 - Sair";
-                cout << "\n Escolha uma opcao e tecle ENTER:";
+                cout << "\n Escolha uma opcao e tecle ENTER: ";
 cin >> opcao;
 
 //sistema de escolhas (sub menu)
 switch(opcao){
 case 1:
-            cout << "Jogo iniciado\n";
+            cout << "\t Jogo iniciado\n\n";
  jogarSozinho();
-           
     break;
 case 2:
             cout << "Informacoes do jogo\n"; 
     break;
 case 3:
             cout << "Ate mais\n";
-    break;
-}}}
+    break;}}}
 
 int main(){
-
+setlocale(LC_ALL, "Portuguese");
 
 srand((unsigned)time(NULL));
 
-menuInicial();
-
-
-}
+menuInicial();}
