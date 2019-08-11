c95b767119db7a62851e00fd824e5bf46d17e17b
caio <caiolimeira10@hotmail.com>
2019-08-11 11:04:56 -0300

    alpha 1.1

--- games/jogopalavras.cpp
+++ games/jogopalavras.cpp
@@ -5,7 +5,8 @@
 #include <iostream>
 #include <string>
 #include <time.h>
-
+#include <windows.h>
+#include <tchar.h>
 using namespace std;
 
 //funçao limpa tela
@@ -14,96 +15,101 @@ void limpaTela(){
 
 //funcao palavra aleatoria
 string rotornaPalavraAleatoria(){
-string palavras[3] = {"abacaxi", "manga", "morango"};
-
+    string palavras[3] = {"abacaxi", "manga", "morango"};
 //indice gerado
-          int indiceAleatorio = rand() %3;
+ int indiceAleatorio = rand() %3;
 //retorna a palavra
-            return palavras[indiceAleatorio]; }
+        return palavras[indiceAleatorio]; }
 
 //funcao de mascara
 string retornaPalavraComMascara(string palavra, int tamanhoDaPalavra){
     //declaracao de variaveis
     int cont = 0;
 string palavraComMascara;
-while(cont < tamanhoDaPalavra){
-    palavraComMascara +="_";
-    cont ++;}
-    return palavraComMascara;}
+    while(cont < tamanhoDaPalavra){
+        palavraComMascara +="_";
+         cont ++;}
+           return palavraComMascara;}
 
-//exibe o status
-void exibeStatus(string palavraComMascara, int tamanhoDaPalavra, int tentativasRestantes, string letrasJaArriscadas){
-                    cout << "Palavra: " << palavraComMascara <<"(tamanho:"<< tamanhoDaPalavra << ")";
-                       cout << "\nTentativas restantes:" << tentativasRestantes << "\n\n";
+//funcao exibe o status
+void exibeStatus(string palavraComMascara, int tamanhoDaPalavra, int tentativasRestantes, string letrasJaArriscadas, string mensagem){
+  cout << mensagem;
+    cout << "Palavra: " << palavraComMascara <<"(tamanho:"<< tamanhoDaPalavra << ")";
+         cout << "\nTentativas restantes:" << tentativasRestantes << "\n\n";
 int cont; //exibe as letras arriscadas
-                             cout << "\n letras arriscadas: "; 
+    cout << "\n letras arriscadas: "; 
     for ( cont = 0; cont < letrasJaArriscadas.size(); cont++) {
-              cout << letrasJaArriscadas[cont] << ", ";   }   }
+         cout << letrasJaArriscadas[cont] << ", ";   }   }
 
 //funçao opcao jogar sozinho
-void jogarSozinho(){
-                //palavra a ser advinhada
-                 string palavra = rotornaPalavraAleatoria();
+int jogarSozinho(){
+    setlocale(LC_ALL,"PORTUGUESE");
+ //palavra a ser advinhada
+ string palavra = rotornaPalavraAleatoria();
 //tamanho da palavra
 int tamanhoDaPalavra = palavra.size();
-                // retorno da palavra e o tamanho dela para a funcao etornaPalavraComMascara
-                 string palavraComMascara = retornaPalavraComMascara(palavra, tamanhoDaPalavra); 
-        //declaracao de variaveis gerais
-int tentativas = 0, maximoDeTentativas = 15, cont = 0;
-string letrasJaArriscadas;  //acumula as tentativas
-char letra;                 //letra identificada
-bool jaDigitouLetra = false;        //auxiliar de letra digitada
+   // retorno da palavra e o tamanho dela para a funcao etornaPalavraComMascara
+     string palavraComMascara = retornaPalavraComMascara(palavra, tamanhoDaPalavra); 
+  //declaracao de variaveis gerais
+int tentativas = 0, maximoDeTentativas = 15; // tentativas
+int cont = 0;                                //auxiliar para repetiçao
+int opcao;                                   //opções finais
+string letrasJaArriscadas;                   //acumula as tentativas
+string mensagem;                             //feedback
+char letra;                                  //letra identificada
+bool jaDigitouLetra = false;                 //bool de letra digitada
+bool acertouletra = false;                   //auxiliar de letra acertada
     //sistema de tentativas
- while (palavra != palavraComMascara && maximoDeTentativas - tentativas > 0){
+    while (palavra != palavraComMascara && maximoDeTentativas - tentativas > 0){
      limpaTela();
-        //exibe o status atual do jogo
-exibeStatus(palavraComMascara, tamanhoDaPalavra, maximoDeTentativas - tentativas, letrasJaArriscadas);
-         cout << "\nDigite uma letra:";           
-           cin >> letra;                //leitura de letras
-
-
+     //exibe o status atual do jogo
+exibeStatus(palavraComMascara, tamanhoDaPalavra, maximoDeTentativas - tentativas, letrasJaArriscadas, mensagem);
+    cout << "\nDigite uma letra:";           
+       cin >> letra;                //leitura de letras
 for ( cont = 0; cont < tentativas; cont++){ 
     //percorre as letras ja arriscadas
-        if (letrasJaArriscadas[cont] == letra){
-            cout << "Essa letra ja foi digitada\n";    //se encontrar a letra  //se letra for repetida true
-            jaDigitouLetra = true;  }
-            else{
-                jaDigitouLetra = false; //caso a proxima letra nao seja repetida ele troca para false assim salta para o proximo laço
-            }}
-            
-            
-        if (jaDigitouLetra == false)  {
+      if (letrasJaArriscadas[cont] == letra){
+        mensagem = "Essa letra ja foi digitada\n";    //se encontrar a letra  //se letra for repetida true
+             jaDigitouLetra = true;  }}         
+ if (jaDigitouLetra == false)  {
              //se for uma letra nova
-            letrasJaArriscadas += letra; 
-           
-
-
+     letrasJaArriscadas += letra; 
 //percorre a palavra se acertar a letra
     for (cont = 0; cont < tamanhoDaPalavra; cont++){
-         if (palavra[cont] == letra){
+        if (palavra[cont] == letra){
           //faço a letra aparecer na palavra
-                 palavraComMascara[cont] = palavra[cont]; }}
+           palavraComMascara[cont] = palavra[cont]; 
+           mensagem = "voce acertou uma letra\n";
+           acertouletra = true;
+            }}
       //incrementa tentativas
-        tentativas ++;
-        }}
-        
-
-        if(palavra == palavraComMascara){
+if (acertouletra == false) {
+            mensagem = "voce errou uma letra\n";}
+        tentativas ++;   }
+        acertouletra = false;
+        jaDigitouLetra = false;
+         }
+if(palavra == palavraComMascara){
             limpaTela();
             cout << "Parabens, você venceu\n";
-}else{
+                cout << "deseja reiniciar?";
+                   cout << "\n1 sim - 2 nao\n"; 
+                    cin >> opcao;
+                        return opcao;  }
+            else{
     limpaTela();
-    cout <<"Você perdeu!";
-}}
+             cout <<"Você perdeu!";
+                cout << "deseja reiniciar?";
+                   cout << "\n1 sim - 2 nao\n"; 
+                    cin >> opcao;
+                        return opcao;}}
 
 //funçao menu
 void menuInicial(){
-// opcao do usuario
 //declaracao de variaveis
-int opcao = 0;
+int opcao = 0; //opcao do usuario
 //menu
 while (opcao < 1 || opcao > 3){
-
 //opcoes                                      
 cout << "Bem vindo ao jogo";
     cout << "\n1 - Jogar";
@@ -111,25 +117,27 @@ cout << "Bem vindo ao jogo";
             cout << "\n3 - Sair";
                 cout << "\n Escolha uma opcao e tecle ENTER: ";
 cin >> opcao;
-
 //sistema de escolhas (sub menu)
 switch(opcao){
 case 1:
             cout << "\t Jogo iniciado\n\n";
             limpaTela();
-        jogarSozinho();
+        
+        if (jogarSozinho() == 1)  {
+        limpaTela();
+           menuInicial();}  
     break;
 case 2:
             cout << "Informacoes do jogo\n"; 
     break;
 case 3:
             cout << "Ate mais\n";
-    break;}}
-    }
+    break;}}  }
 
 int main(){
-setlocale(LC_ALL, "PORTUGUESE");
-
+setlocale(LC_ALL,"Brasil");
+_tsetlocale(LC_ALL, _T("portuguese"));
+limpaTela();
 
 srand((unsigned)time(NULL));
 
